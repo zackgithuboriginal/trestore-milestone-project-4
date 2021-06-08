5c3bf2740df67c333f6f891572ceca7bfdbef3b2
zackgithuboriginal <z.gibbons@hotmail.com>
2021-06-08 14:17:36 +0000

    Updated admin name displays for models and set displayed attributes

--- products/models.py
+++ products/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
 class Category(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
-    
+
     def get_friendly_name(self):
         return self.friendly_name
 
+
 class Product(models.Model):
-    category  = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     product_name = models.CharField(max_length=254)
     sku =  models.CharField(max_length=254, null=True, blank=True)
     product_description = models.TextField()
@@ -21,4 +26,4 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.product_name
