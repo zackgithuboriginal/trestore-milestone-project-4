21d7ae8e926dd89ba7f25f921febc682504eee02
zackgithuboriginal <z.gibbons@hotmail.com>
2021-06-09 14:16:23 +0000

    Added store page with basic product layout and url routing

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    """ This view will display all products """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
