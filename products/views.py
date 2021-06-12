cdd1e885b8bcb2b7523c12d871b0db73bbc401b7
zackgithuboriginal <z.gibbons@hotmail.com>
2021-06-12 12:30:28 +0000

    Added product Sorting, sort and filter parameters will now display on results page

--- products/views.py
+++ products/views.py
@@ -10,15 +10,19 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    category = None
+    category_friendly = 'All Categories'
+    current_category = None
+    current_sorting = 'Price Ascending'
 
     if request.GET:
         if 'product-filter' in request.GET:
             if request.GET['product-filter'] == 'all':
-                pass
+                category = None
             else:
                 category = request.GET['product-filter']
+                current_category = category
                 products = products.filter(category__name=category)
+                category_friendly = Category.objects.get(name=category)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -28,10 +32,27 @@ def all_products(request):
             queries = Q(product_name__icontains=query) | Q(product_description__icontains=query)
             products = products.filter(queries)
 
+        if 'product-sort' in request.GET:
+            sortkey = request.GET['product-sort']
+            print(sortkey)
+            if sortkey == 'price_asc':
+                products = products.order_by('price')
+                current_sorting = 'Price Ascending'
+            elif sortkey == 'price_desc':
+                sort_direction = '-price'
+                products = products.order_by(sort_direction)
+                current_sorting = 'Price Descending'
+            else:
+                products = products.order_by('category')
+                current_sorting = 'Category'
+
     context = {
         'products': products,
         'search_term': query,
-        'current_category': category,
+        'category_friendly': category_friendly,
+        'current_category': current_category,
+        'current_sorting': current_sorting,
+
     }
 
     return render(request, 'products/products.html', context)
