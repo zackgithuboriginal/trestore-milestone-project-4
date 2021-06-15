9f2d72dbfc0daef4b68b0319695b8552b7fa7abd
zackgithuboriginal <z.gibbons@hotmail.com>
2021-06-15 12:38:38 +0000

    Added basket items context processor to update grand total display and enable rendering of basket items

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,11 +1,23 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def basket_contents(request):
 
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     delivery_cost = total * Decimal(settings.DELIVERY_PERCENTAGE)
 
@@ -20,6 +32,7 @@ def basket_contents(request):
         'delivery_cost': delivery_cost,
         'tree_planting_contribution': tree_planting_contribution,
         'tree_planting_percentage': settings.TREE_PLANTING_PERCENTAGE,
+        'grand_total': grand_total,
     }
 
 
