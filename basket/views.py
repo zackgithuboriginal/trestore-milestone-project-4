08d50dec04be5d870b8b3f44461a420102db885d
zackgithuboriginal <z.gibbons@hotmail.com>
2021-06-15 10:07:37 +0000

    Added add to basket session functionality

--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def view_basket(request):
     """A view thata renders the basket display page """
-    
+
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """Add a specific product to basket"""
+
+    quantity = int(request.POST.get('quantity'))
+
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect('products')
