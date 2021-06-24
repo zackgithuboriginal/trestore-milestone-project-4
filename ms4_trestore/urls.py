fa9f2306042414a86e310c9d614221047327ad66
zackgithuboriginal <z.gibbons@hotmail.com>
2021-06-24 16:33:39 +0000

    Created checkout app urls.py and url routing

--- ms4_trestore/urls.py
+++ ms4_trestore/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('sponsorship-packages/', include('sponsor.urls')),
     path('basket/', include('basket.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
