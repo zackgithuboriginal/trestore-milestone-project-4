346d462c9dbb912de8906d0c468c12ef3df0e8eb
zackgithuboriginal <z.gibbons@hotmail.com>
2021-06-26 15:29:59 +0000

    Setup stripe card element and order creation

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,38 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 # Create your views here.
 
+
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "You need to have items in your basket to checkout.")
         return redirect(reverse('products'))
     
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
