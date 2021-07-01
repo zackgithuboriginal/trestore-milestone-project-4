c6acdf8c37e559385801a89fc783bb6d4445a42a
zackgithuboriginal <z.gibbons@hotmail.com>
2021-07-01 12:13:49 +0000

    Replaced Secret key with value in heroku config cars and set debug

--- ms4_trestore/settings.py
+++ ms4_trestore/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$k=w9uxyr6i$*@4dmg@v&r4!x*f@ct^=usl_8&0f@fx)svn@8n'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['trestore.herokuapp.com', 'localhost']
 
