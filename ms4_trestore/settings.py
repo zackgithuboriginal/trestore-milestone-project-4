343b84e15c6880227a1a1a49d6a3f380f84d464b
zackgithuboriginal <z.gibbons@hotmail.com>
2021-07-04 16:11:43 +0000

    Set up user profiles app and created userprofile model reflected in order models

--- ms4_trestore/settings.py
+++ ms4_trestore/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'sponsor',
     'basket',
     'checkout',
+    'user_profiles',
 
     "crispy_forms",
     "crispy_bootstrap5",
