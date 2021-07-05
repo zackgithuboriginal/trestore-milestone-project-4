b7d52559308cd3d5823ecff362e08bee63ca29c4
zackgithuboriginal <z.gibbons@hotmail.com>
2021-07-05 20:26:20 +0000

    Fixed issue with media directory not being specified in context processor

--- ms4_trestore/settings.py
+++ ms4_trestore/settings.py
@@ -5,7 +5,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -73,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'basket.contexts.basket_contents',
             ],
             'builtins': [
