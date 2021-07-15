294a989cefa4a90b06b4e9df3d737d36d8cbfe58
zackgithuboriginal <z.gibbons@hotmail.com>
2021-07-15 15:29:51 +0000

    Added edit/delete options to template

--- static/js/base.js
+++ static/js/base.js
@@ -1,8 +1,8 @@
 /**
  * This function handles the display of the post edit options dropdown menu
  */
- function productOptionsDropdown(product_id) {
-    let target = document.getElementById(`product-options-${product_id}`);
+ function displayOptionsDropdown(id) {
+    let target = document.getElementById(`display-options-${id}`);
     if (target.style.display == "flex") {
         clearDropdowns();
         target.style.display = "none";
@@ -16,7 +16,7 @@
  * This function clears all of the post edit option dropdowns to ensure no more than one displays at a time
  */
 function clearDropdowns() {
-    let allDropdowns = document.querySelectorAll(".product-edit-options");
+    let allDropdowns = document.querySelectorAll(".display-edit-options");
     for (let i = 0; i < allDropdowns.length; i++) {
         allDropdowns[i].style.display = "none";
     }
