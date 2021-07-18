5bc6f5bdcbcf58ebd8a2210f69a40d2497b1ec80
zackgithuboriginal <z.gibbons@hotmail.com>
2021-07-18 20:55:57 +0000

    Fixed issue with product adding returning 500 error

--- ms4_trestore/settings.py
+++ ms4_trestore/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'x291sd=eklir97tdg)og82s&1g$i8mjwy)+o&!+rrwla#y%!ew')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['trestore.herokuapp.com', 'localhost']
 
--- products/forms.py
+++ products/forms.py
@@ -13,9 +13,14 @@ class AddProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
+            if field_name == 'price':
+                field.widget.attrs['min'] = '0'
+            if field_name == 'product_description':
+                field.widget.attrs['rows'] = '4'
             field.widget.attrs['class'] = 'stripe-style-input'
