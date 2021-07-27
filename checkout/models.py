b97be84a59972a976e6f12be8f18a24a7b0fcdae
zackgithuboriginal <z.gibbons@hotmail.com>
2021-07-27 16:11:12 +0000

    Small change to lineitem max digits to prevent errors at large numbers

--- checkout/models.py
+++ checkout/models.py
@@ -85,7 +85,7 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(null=False,
                                    blank=False,
                                    default=0)
-    lineitem_total = models.DecimalField(max_digits=6,
+    lineitem_total = models.DecimalField(max_digits=10,
                                          decimal_places=2,
                                          null=False,
                                          blank=False,
