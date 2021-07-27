a083f6bc15fffe60049f7616e2dbb7baa79f44df
zackgithuboriginal <z.gibbons@hotmail.com>
2021-07-27 15:46:37 +0000

    Fixed issue with order total calcualion

--- checkout/models.py
+++ checkout/models.py
@@ -50,8 +50,9 @@ class Order(models.Model):
         self.order_total = self.lineitems.aggregate(Sum(
                                                     'lineitem_total'))[
                                                     'lineitem_total__sum'] or 0
-        self.delivery_cost = self.order_total * (settings.DELIVERY_PERCENTAGE /
-                                                 100)
+        self.delivery_cost = ((self.order_total
+                              * settings.DELIVERY_PERCENTAGE)
+                              / 100)
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
