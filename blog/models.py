c4a9a7819ca7db70e5adbde7d37d62c598993606
zackgithuboriginal <z.gibbons@hotmail.com>
2021-07-29 11:56:56 +0000

    Updated blog post model field type

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class ProgressPost(models.Model):
     """
 
     post_title = models.CharField(max_length=60, null=False, blank=False)
-    post_content = models.CharField(max_length=2000, null=False, blank=False)
+    post_content = models.TextField(max_length=2000, null=False, blank=False)
     image = models.ImageField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True, editable=False)
     author = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
