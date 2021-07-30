3a7a96f77c759a046943eee4a4596fd4bdb64993
zackgithuboriginal <z.gibbons@hotmail.com>
2021-07-30 17:54:23 +0000

    Formatted js, remvoed inline js

--- user_profiles/static/user_profiles/js/user_profiles.js
+++ user_profiles/static/user_profiles/js/user_profiles.js
@@ -2,17 +2,18 @@
  * Toggles between showing the user's delivery details or the
  * delivery details input form, if a user clicks the edit button
  */
-function toggleForm(event) {
-    if (event.firstChild.className === 'fas fa-edit') {
-        event.firstChild.className = 'fas fa-times';
-        event.firstChild.style.color = '#CC2936';
-        event.parentNode.parentNode.parentNode.children[1].style.display = 'none';
-        event.parentNode.parentNode.parentNode.children[2].style.display = 'flex';
+ $('#toggle-delivery-details').click(function(){
+    let target = $(this);
+    if (target.children(":first").attr('class') === 'fas fa-edit') {
+        target.children(":first").attr('class', 'fas fa-times');
+        target.children(":first").css('color', '#CC2936');
+        $('#details-display').css('display', 'none');
+        $('#details-form').css('display', 'flex');
 
     } else {
-        event.firstChild.className = 'fas fa-edit';
-        event.firstChild.style.color = '#336857';
-        event.parentNode.parentNode.parentNode.children[1].style.display = 'flex';
-        event.parentNode.parentNode.parentNode.children[2].style.display = 'none';
+        target.children(":first").attr('class', 'fas fa-edit');
+        target.children(":first").css('color', '#336857');
+        $('#details-display').css('display', 'flex');
+        $('#details-form').css('display', 'none');
     }
-}
+});
