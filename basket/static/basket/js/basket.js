a3b22f271d5051d6a7a92d7b19f564699c924d16
zackgithuboriginal <z.gibbons@hotmail.com>
2021-07-30 18:01:17 +0000

    Added comments to js file

--- basket/static/basket/js/basket.js
+++ basket/static/basket/js/basket.js
@@ -1,13 +1,22 @@
+/**
+ * Handles the update product form submission from the larger page view
+ */
 $('.update-basket-button').click(function (e) {
     var form = $(this).prev('.update-quantity-form');
     form.submit();
 });
 
+/**
+ * Handles the update product form submission from the small page view
+ */
 $('.update-basket-button-sm').click(function (e) {
     var form = $(this).prev('.update-quantity-form');
     form.submit();
 });
 
+/**
+ * Handles the delete product request submission from the larger page view
+ */
 $('.basket-product-delete-tb').click(function (e) {
     var csrfToken = $('.basket-product-delete-tb').attr('data-csrf');
     var itemId = $(this).attr('id').split('delete_')[1];
@@ -22,6 +31,9 @@ $('.basket-product-delete-tb').click(function (e) {
         });
 });
 
+/**
+ * Handles the delete product request submission from the sm page view
+ */
 $('.basket-product-delete-sm').click(function (e) {
     var csrfToken = $('.basket-product-delete-sm').attr('data-csrf');
     var itemId = $(this).attr('id').split('delete_sm_')[1];
